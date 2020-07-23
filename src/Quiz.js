5e99d4d94942a8d06ec6e791f9679d291ada0638
vipulahuja <vipulahuja1997@gmail.com>
2020-07-23 18:53:00 +0530

    commit2

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,7 +2,7 @@ import React,{ Component} from 'react';
 
 let quizData = require('./quiz_data.json');
 
-export default class Quiz extends Component{
+class Quiz extends Component{
     constructor(props){
         super(props);
         this.state = {
@@ -19,3 +19,5 @@ export default class Quiz extends Component{
         )
     }
 }
+
+export default Quiz;
